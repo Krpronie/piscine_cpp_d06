41126ad6bee69658269130e129a7c93656acb4c2
Krpronie <dlhp222@163.com>
2018-07-10 10:14:37 +0800

    02

---
+++ ex02/AssaultTerminator.hh
@@ -0,0 +1,18 @@
+#ifndef ASSAULTTERMINATOR_H_
+#define ASSAULTTERMINATOR_H_
+
+#include "ISpaceMarine.hh"
+
+class AssaultTerminator : public ISpaceMarine
+{
+	public:
+		explicit AssaultTerminator();
+		virtual ~AssaultTerminator();
+
+		ISpaceMarine* clone() const;
+		void battleCry() const;
+		void rangedAttack() const;
+		void meleeAttack() const;
+};
+
+#endif
